eeef7239889cceb1022d1edd970efa42f6dc2544
Kaylah Malillos <malillosk@gmail.com>
2019-05-10 20:33:26 -0600

    updateStorage and renderItems functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,35 +16,54 @@ if (!Array.isArray(budgetItems)) {
 // ======================
 
 // 4th: update localStorage with latest budgetItems and latest lastID
+function updateStorage() {
+    localStorage.setItem("budgetItems", JSON.stringify(budgetItems));
+    localStorage.setItem("lastID", lastID);
+};
 
 // 5th: render budgetItems on table
+function renderItems(items) {
+    if (!items) {
+        items = budgetItems;
+    }
 
+    var tbody = $("#budgetItems tbody");
+    // clear out previous budget items
+    tbody.empty();
 
+    // make table row for each budget item
+    items.forEach(function (item) {
+        var row = `<tr><td>${item.date}</td><td>${item.name}</td><td>${item.category}</td><td>${item.amount}</td><td>${item.notes}</td><td class="delete">X</td></tr>`
+        tbody.prepend(row);
+    });
+};
 
 // ======================
 // MAIN PROCESS
 // ======================
 
+renderItems();
+
 // 2nd: wire up click event on 'Enter New Budget Item' button to toggle display of form
-$("#toggleFormButton").on("click", function() {
+$("#toggleFormButton").on("click", function () {
     var button = $("#toggleFormButton");
     var form = $("#addItemForm");
 
-    form.toggle("slow", function() {
-      if ($(this).is(":visible")) {
-          button.text("Hide Form");
-      } else {
-          button.text("Enter New Budget Item");
-      }
+    form.toggle("slow", function () {
+        if ($(this).is(":visible")) {
+            button.text("Hide Form");
+        } else {
+            button.text("Enter New Budget Item");
+        }
     });
 });
 
 // 3rd: on click of 'Add Budget Item' button, gather user input and add newest item to budgetItems array
 // each budget item should include: id / date / name / category / amount / notes
 // (also, trigger localStorage update and trigger budgetItems rerender, once created)
-$("#addItem").on("click", function(event) {
+$("#addItem").on("click", function (event) {
     event.preventDefault();
-  
+
     var budgetItem = {
         id: ++lastID,
         date: moment().format('LLL'),
@@ -56,6 +75,8 @@ $("#addItem").on("click", function(event) {
 
     console.log(budgetItem);
     budgetItems.push(budgetItem);
+
+    updateStorage();
 
     // clears form; .val("") -> sets whatever is inside
     $("#addItemForm input, #addItemForm select").val("");
